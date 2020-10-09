dbbd224090df4146f9ad66a2d91edd1c64362481
ethicahacker <72151850+ethicahacker@users.noreply.github.com>
2020-10-09 15:06:36 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,25 +5,13 @@ c = 0
 while c < 5:
     x = random.randint(1, 50)
 
-    n = eval(input("Enter your guess between 1 to 50: "))
+    n = int(input("Enter your guess between 1 to 50: "))
 
-    if n < x or :
-        print("You choice very small number")
+    if n > x:
+        print("You choose bigger number")
 
-     elif n > (x + 5):
-         print("You are very near to the number")
-    
-     elif n > (x + 3):
-         print("you are near to the number")
-
-    elif n > x:
-        print("you choice is bigger than the number")
-
-     elif n < (x + 5):
-         print("you are very after the number")
-    
-     elif n < (x + 3):
-         print("you are a little after the number")
+    elif n < x:
+        print("you choose smaller number")
 
     elif n == x:
         print("Congrats! you guessed the number ")
